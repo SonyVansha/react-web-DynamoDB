b304924b96859b98967a37a250a24cec2be2e688
SonyVansha <sonyvansha@gmail.com>
2024-02-16 08:42:16 +0700

    first commit

--- README.md
+++ README.md
@@ -39,32 +39,22 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
-## Learn More
+# React To AWS code
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+#### Install UI MUI
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+```sh
+npm install @mui/material @emotion/react @emotion/styled
+```
 
-### Code Splitting
+#### Install package
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+```sh
+npm install
+```
 
-### Analyzing the Bundle Size
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+#### Start React Web
+```sh
+npm start
+```
---
+++ src/components/Users.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+
+import { Box, TableHead, Typography, Table, TableRow, TableCell, TableBody, styled, Button } from '@mui/material';
+
+const Component = styled(Box)`
+    width: 80%;
+    margin: 50px auto;
+    & > h4 {
+        margin-bottom: 20px;
+    };
+    & > div > table > thead {
+        background-color: #000
+    }
+    & > div > table > thead > tr > th {
+        color: #FFFFFF;
+        font-size: 20px;
+        font-weight: 600;
+    }
+    & > div > table > tbody > tr > td {
+        font-size: 20px;
+    }
+`;
+
+const defaultObj = [{
+    id: 101,
+    name: 'Sony Vansha',
+    email: 'example@gmail.com',
+    phone: '6176412798',
+    salary: 222000,
+    age: 19
+}];
+
+const Users = () => {
+
+    const [users, setUsers] = useState(defaultObj);
+
+    const removeEntry = (id) => {
+        const UpdateUsers =  users.filter(user => user.id !== id);
+        setUsers(UpdateUsers);
+    }
+
+    return (
+        <Component>
+            <Typography variant='h4'>Users</Typography>
+            <Box>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Phone</TableCell>
+                            <TableCell>Salary</TableCell>
+                            <TableCell>Age</TableCell>
+                            <TableCell>Remove Entry</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {
+                            users.map( user => (
+                                <TableRow key={user.id}>
+                                    <TableCell>{user.id}</TableCell>
+                                    <TableCell>{user.name}</TableCell>
+                                    <TableCell>{user.email}</TableCell>
+                                    <TableCell>{user.phone}</TableCell>
+                                    <TableCell>{user.salary}</TableCell>
+                                    <TableCell>{user.age}</TableCell>
+                                    <TableCell><Button variant='contained' color='error' onClick={() => removeEntry(user.id)}>Remove</Button></TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </Box>
+        </Component>
+    )
+}
+
+export default Users;
